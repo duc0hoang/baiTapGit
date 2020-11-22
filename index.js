fcaf687c5197c920d9ab3b69070f3299b44b5e44
duc0hoang <duc0hoang@gmail.com>
2020-11-22 22:26:08 +0700

    'version6_merge_2_nhanh'

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ console.log("Day la nhanh master");
 
 console.log("f_initBranch");
 
-<<<<<<< HEAD
 console.log("f1");
 
 console.log("done f2");
-=======
 console.log("F2");
 
 console.log("Done F2");
->>>>>>> Feture2
